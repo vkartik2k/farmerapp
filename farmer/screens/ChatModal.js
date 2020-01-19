d50c90aa96315049d2488bc6c2f8c2424492ccb7
vkartik2k <vkartik2k@gmail.com>
2020-01-19 18:03:17 +0530

    chatModal

--- farmer/screens/ChatModal.js
+++ farmer/screens/ChatModal.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View, TextInput, Modal, Dimensions, Button } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons'
+import { StyleSheet, Text, View, TextInput, Modal, Dimensions, ScrollView } from 'react-native'
+import { MaterialIcons, FontAwesome } from '@expo/vector-icons'
+import { Linking } from 'expo';
 
 let height = Dimensions.get('window').height
 let width = Dimensions.get('window').width
@@ -64,22 +65,83 @@ export default class ChatModal extends React.Component {
           </View>
           <View style={styles.main}>
             <View style={styles.chatInput}>
-              <TextInput style={styles.input} placeholder="Type your message ..." />
-              <View style={{backgroundColor: 'white', borderRadius: 30,padding: 8, marginRight:10}}>
+              <TextInput style={styles.input} placeholder="Type your message" />
+              <View style={{
+                backgroundColor: '#0A79DF',
+                borderRadius: 30,
+                padding: 8,
+                marginRight: 10,
+                shadowColor: '#000000',
+                elevation: 1,
+                shadowRadius: 5,
+                shadowOpacity: 0.3,
+              }}>
                 <MaterialIcons
                   name="send"
-                  color="#0A79DF"
+                  color="#fff"
                   size={30}
                   onPress={() => this.props.closeDisplay()}
                 />
               </View>
             </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+              <ScrollView horizontal={true} >
+                <View style={styles.additionalMsg}>
+                  <Text>Request for phone no.</Text>
+                </View>
+                <View style={styles.additionalMsg}>
+                  <Text>Fixed Price</Text>
+                </View>
+                <View style={styles.additionalMsg}>
+                  <Text>Schedule Meeting</Text>
+                </View>
+                <View style={styles.additionalMsg}>
+                  <Text>Request for phone no.</Text>
+                </View>
+              </ScrollView>
+            </View>
             <View style={styles.chatBox}>
-              <SendMsg msg="Hello"/>
-              <RecMsg msg="Deal done bla bla bla sa dsgsgsd"/>
-              <RecMsg msg="LOREM IPSUM DSG FDHDHG DGDGFDHFGDFDGSFDHGFHFGDF FDGHGDFDGDG "/>
-              <RecMsg msg="dsf bla bldsfsgd"/>
-              <SendMsg msg="Hello"/>
+              <ScrollView>
+                <SendMsg msg="Hello" />
+                <RecMsg msg="Deal done bla bla bla sa dsgsgsd" />
+                <RecMsg msg="LOREM IPSUM DSG FDHDHG DGDGFDHFGDFDGSFDHGFHFGDF FDGHGDFDGDG " />
+                <RecMsg msg="dsf bla bldsfsgd" />
+                <SendMsg msg="Hello" />
+                <SendMsg msg="Hello" />
+                <RecMsg msg="Deal done bla bla bla sa dsgsgsd" />
+                <RecMsg msg="LOREM IPSUM DSG FDHDHG DGDGFDHFGDFDGSFDHGFHFGDF FDGHGDFDGDG " />
+                <RecMsg msg="dsf bla bldsfsgd" />
+                <SendMsg msg="Hello" />
+                <SendMsg msg="Hello" />
+                <RecMsg msg="Deal done bla bla bla sa dsgsgsd" />
+                <RecMsg msg="LOREM IPSUM DSG FDHDHG DGDGFDHFGDFDGSFDHGFHFGDF FDGHGDFDGDG " />
+                <RecMsg msg="dsf bla bldsfsgd" />
+                <SendMsg msg="Hello" />
+              </ScrollView>
+            </View>
+            <View style={styles.informationCard}>
+              <View>
+                <Text style={{ fontSize: 16 }}>Name : Ram Peela Meena</Text>
+                <Text style={{ fontSize: 16 }}>Mobile No. : 9999999999</Text>
+                <Text style={{ fontSize: 16 }}>Location : North Delhi</Text>
+              </View>
+              <View style={{
+                padding: 10,
+                paddingHorizontal: 14,
+                borderRadius: 60,
+                backgroundColor: '#6AB04A',
+                shadowColor: '#000000',
+                elevation: 7,
+                shadowRadius: 5,
+                shadowOpacity: 0.3,
+              }}>
+                <FontAwesome
+                  name="phone"
+                  color="#fff"
+                  size={35}
+                  onPress={() => Linking.openURL('tel:+123456789')}
+                />
+              </View>
             </View>
           </View>
         </View>
@@ -93,7 +155,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#DAE0E2',
     alignItems: 'center',
-    paddingTop: 60
+    paddingTop: 53
   },
   headerContainer: {
     zIndex: 9,
@@ -123,7 +185,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    width: width
+    width: width,
   },
   input: {
     backgroundColor: '#fff',
@@ -131,17 +193,25 @@ const styles = StyleSheet.create({
     fontSize: 18,
     flex: 1,
     margin: 10,
-    borderRadius: 30
+    borderRadius: 30,
+    shadowColor: '#000000',
+    elevation: 1,
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
   },
   RecMsg: {
     padding: 10,
     maxWidth: 270,
     backgroundColor: '#FFFFFF',
     borderRadius: 30,
-    borderTopLeftRadius : 5,
+    borderTopLeftRadius: 5,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-end',
+    shadowColor: '#000000',
+    elevation: 1,
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
   },
   SendMsg: {
     padding: 10,
@@ -152,6 +222,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'flex-end',
+    shadowColor: '#000000',
+    elevation: 1,
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
   },
   SendMsgContainer: {
     padding: 2,
@@ -161,9 +235,28 @@ const styles = StyleSheet.create({
     padding: 2,
     flexDirection: 'row',
   },
-  timeStamp: {
-    fontSize: 12,
-    color: "#7B8788",
-    paddingLeft: 10,
+  additionalMsg: {
+    backgroundColor: '#e8f4f8',
+    borderRadius: 30,
+    padding: 10,
+    marginHorizontal: 5,
+    marginTop: 5,
+    shadowColor: '#000000',
+    elevation: 1,
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
   },
+  informationCard: {
+    backgroundColor: '#e8f4f8',
+    padding: 10,
+    margin: 10,
+    borderRadius: 8,
+    shadowColor: '#000000',
+    elevation: 2,
+    shadowRadius: 5,
+    shadowOpacity: 1.0,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: 'space-between',
+  }
 })
